103c8c8bb8b254a96089cfd5f5247d064ab0dc29
Kishan2029 <kishanprajapati2029@gmail.com>
2021-09-29 15:19:29 +0530

    Add, Update and Delete Post functionalaity is added

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
-import { getPosts, createPosts } from '../controllers/posts.js'; 
+import { getPosts, createPosts, updatePost, deletePost } from '../controllers/posts.js'; 
 
 const router = express.Router();
 
+
+
 router.get('/', getPosts);
 router.post('/', createPosts);
+// router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+// router.patch('/:id/likePost', likePost);
 
 export default router;
